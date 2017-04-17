74a219d8e027c11f5531b40dd55bd44e8e3090ce
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-04-17 18:37:03 +0000

    updated phone number

--- client/send-sms.js
+++ client/send-sms.js
@@ -12,6 +12,6 @@ client.messages
     messaging_service_sid: 'MGf35b578dd42e5d59e07e2bbd0628dfb0',
     uri: '/2010-04-01/Accounts/AC8c3504522c520c193783e64e0d493739/Messages/SM1fed2c6b6c6a4c63911c078811efdd2a.json',
     direction: 'outbound-api',
-    body: 'A little applet. Reply to this text with either Parsnip, Celery, or Puffer.',
+      body: 'Reply to this text with either Parsnip, Celery, or Puffer.',
   })
   .then((message) => console.log(message.sid,message.status));
